227b4b511372767e7788885cf7ecbdb90012c098
sahar-letaief <sahar.letaief@esprit.tn>
2023-05-09 13:52:12 +0100

    footer + header + acceuil done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Findpharma'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Findpharma');
+    expect(app.title).toEqual('PharmaLife');
   });
 
   it('should render title', () => {
